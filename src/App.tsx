e2cfb13d1adde39968bf42c6374bd12eb2330ebc
Renato Xavier <rnatu91@gmail.com>
2020-12-22 16:16:02 -0300

    Typescript e React - useInterval (React Hook)
    Aula sobre a utilização do useInterfacel (React Hook) para utilização de timer no react

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <h1>Olá mundo</h1>
+      <PomodoroTimer defaultPomodoroTime={1500} />
     </div>
   );
 }
---
+++ src/components/pomodoro-timer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useInterval } from '../hooks/use-interval';
+
+interface Props {
+  defaultPomodoroTime: number;
+}
+
+export function PomodoroTimer(props: Props): JSX.Element {
+  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+
+  useInterval(() => {
+    setMainTime(mainTime - 1);
+  }, 1000);
+
+  return (
+    <div>
+      <h1>{mainTime}</h1>
+    </div>
+  );
+}
