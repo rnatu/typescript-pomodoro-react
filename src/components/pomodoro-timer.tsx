9555c2967f8d8a6d24144d4d1d3d2bf42b41b3c6
Renato Xavier <rnatu91@gmail.com>
2020-12-23 14:04:21 -0300

    Typescript e React - Configurando o CSS
    Aula realizando a estilização do CSS do projeto pomodoro

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -4,11 +4,14 @@ import { Button } from './button';
 import { Timer } from './timer';
 
 interface Props {
-  defaultPomodoroTime: number;
+  pomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+  const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
@@ -18,7 +21,16 @@ export function PomodoroTimer(props: Props): JSX.Element {
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTime={mainTime} />
-      <Button text="teste" onClick={() => console.log(1)}></Button>
+
+      <div className="controls">
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+        <Button text="teste" onClick={() => console.log(1)}></Button>
+      </div>
+
+      <div className="details">
+        <p>Testando: hsdahsuidhasdu asuidaushdi saidh9</p>
+      </div>
     </div>
   );
 }
