2fa8fbdcf8af9a2ba00f815ce5df482fb14a9940
Renato Xavier <rnatu91@gmail.com>
2021-01-04 15:37:45 -0300

    Typescript e React - Coriggindo "Horas trabalhadas"
    Realizada a correção de horas trabalhadas para a exibição correta

--- src/utils/seconds-to-time.ts
+++ src/utils/seconds-to-time.ts
@@ -1,8 +1,9 @@
-export function secondsToTime(seconds: number): string {
-  const zeroLeft = (n: number) => Math.floor(n).toString().padStart(2, '0');
+import { zeroLeft } from './zero-left';
 
+export function secondsToTime(seconds: number): string {
+  const hour = zeroLeft(seconds / 3600);
   const min = zeroLeft((seconds / 60) % 60);
   const sec = zeroLeft((seconds % 60) % 60);
 
-  return `${min}:${sec}`;
+  return `${hour}h:${min}m:${sec}s`;
 }
